acab8e502b490048e8941e685a1bb7e3c1ee85b4
Dakota McKay <dakotamckay72@gmail.com>
2024-08-26 17:57:04 -0400

    Write a request a webpage, download the webpage and only request the page if it is not downloaded.

---
+++ 04_scraping_js/main.js
@@ -0,0 +1,72 @@
+import { setTimeout } from "timers/promises";
+import { fileURLToPath } from 'url';
+import * as cheerio from 'cheerio';
+import { dirname } from 'path';
+import axios from 'axios';
+import path from 'path';
+import fs from 'fs';
+
+
+const filename = fileURLToPath(import.meta.url);
+const dir = dirname(filename);
+const filepath = path.join(dir, 'page.html');
+
+
+// Define the extensions of the files desired
+const desiredExtensions = new Set([
+    'fed', 
+    'fianl sauser musquily 1', 
+    'axel-lonewolf)(v14)(',
+]);
+
+// Define default download headers
+const download_config = {
+    headers: {
+        'Referer': 'http://acmlm.kafuka.org/uploader/index.php?act=viewcat&id=25',
+    }
+}
+
+
+// Fetch the page
+async function fetchPage() {
+    try {
+        const response = await axios.get(target);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching the page: ', error);
+        throw error;
+    }
+};
+
+
+// Save Content to a file
+function saveToFile(content, filepath) {
+    fs.writeFileSync(filepath, content);
+    console.log("Page saved to ", filepath);
+};
+
+
+// Read data from file
+function readFromFile(filePath) {
+    try {
+        const content = fs.readFileSync(filePath);
+        console.log('Page loaded from ', filePath);
+        return content;
+    } catch(error) {
+        console.error('Error reading the file: , error');
+        throw error;
+    }
+};
+
+
+// Function to get page
+async function getPage() {
+    if (fs.existsSync(filepath)) {
+        return readFromFile(filepath);
+    } else {
+        const pageContent = await fetchPage();
+        saveToFile(pageContent, filepath);
+        return pageContent;
+    }
+};
+
