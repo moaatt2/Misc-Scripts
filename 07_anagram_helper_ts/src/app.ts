9cbdc39006f71a97dc71a0e15f51e02c4ad818a7
Dakota McKay <dakotamckay72@gmail.com>
2026-05-25 07:21:05 -0400

    Take input and relabel to cypher text and work to calculate the number of possible options.

--- 07_anagram_helper_ts/src/app.ts
+++ 07_anagram_helper_ts/src/app.ts
@@ -1,11 +1,30 @@
 import { createInterface } from 'node:readline';
+import { factorial } from 'mathjs';
 
+// Create interface for taking user input
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-rl.question(`What's your name?`, (name: string) => {
-    console.log(`Hello, ${name}!`);
+
+// create function to process provided cypher text
+function processAnagram(cypherText: string): void {
+
+    // Confirm the user provided text
+    console.log(`You provided the following cypher text: ${cypherText}`);
+
+    // Calculate number of possibilities
+    // For a string of length n, the number of possible arrangements is n!
+    let possibilities = factorial(cypherText.length);
+
+    // Inform user of the number of possibilities
+    console.log(`The are ${possibilities} possible arrangements of the provided cypher text.`);
+}
+
+
+// Get Scrambled text from user
+rl.question(`What is the cipher text: `, (text: string) => {
+    processAnagram(text);
     rl.close();
 })
