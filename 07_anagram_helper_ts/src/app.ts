732e4bae76c5a88e0a7354891545f49c4394b1ba
Dakota McKay <dakotamckay72@gmail.com>
2026-05-30 07:40:58 -0400

    Ensure that spaces show up in the output, and streamline flow since spaces get counted and removed.

--- 07_anagram_helper_ts/src/app.ts
+++ 07_anagram_helper_ts/src/app.ts
@@ -24,17 +24,25 @@ String.prototype.count = function (char: string): number {
 
 
 // Use a recursive function to generate all arrangements
-function recursiveAnagram(current: string, remaining: string): void {
+function recursiveAnagram(current: string, remaining: string, spaces: Set<number>): void {
+
+    // Base Case
     if (remaining.length === 0 && !global.resultSet.has(current)) {
         console.log(`Found arrangement: ${current}`);
         global.resultSet.add(current);
         return;
     }
 
+    // Add a space if necessary
+    if (spaces.has(current.length)) {
+        current += ' ';
+    }
+
+    // Main Recursive Case
     for (let i = 0; i < remaining.length; i++) {
         const next = current + remaining[i];
         const nextRemaining = remaining.slice(0, i) + remaining.slice(i + 1);
-        recursiveAnagram(next, nextRemaining);
+        recursiveAnagram(next, nextRemaining, spaces);
     }
 }
 
@@ -48,16 +56,27 @@ function processAnagram(cypherText: string): void {
     // Confirm the user provided text
     console.log(`You provided the following cypher text: ${cypherText}`);
 
+    // Get array of space indicies
+    let indicies: Set<number> = new Set<number>();
+    for (let i=0; i < cypherText.length; i++) {
+        if (cypherText[i] === ' ') {
+            indicies.add(i);
+        }
+    }
+
+    // Remove spaces from cypher text for processing
+    cypherText = cypherText.replace(/ /g, '');
+
     // Calculate number of possibilities
     // For a string of length n, the number of possible arrangements is n!
     // Ignore spaces as they are in fixed positions
-    let possibilities = factorial(cypherText.length - cypherText.count(' '));
+    let possibilities = factorial(cypherText.length);
 
     // Duplicate Characters reduce the number of possibilities
     //   for each non-space character divide the total possibilities by the factorial of the number of characters
     let uniqueChars: Set<string> = new Set(cypherText);
     uniqueChars.forEach((char: string) => {
-        if (char !== ' ' && cypherText.count(char) > 1) {
+        if (cypherText.count(char) > 1) {
             possibilities /= factorial(cypherText.count(char));
         }
     });
@@ -66,7 +85,7 @@ function processAnagram(cypherText: string): void {
     console.log(`The are ${possibilities} possible arrangements of the provided cypher text.`);
 
     // Start running recursive function to print arrangements
-    recursiveAnagram('', cypherText);
+    recursiveAnagram('', cypherText, indicies);
 }
 
 
